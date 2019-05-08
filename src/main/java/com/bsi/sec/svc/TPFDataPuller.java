b3edbc06f041005a565a74a5f2d05c0fc12374a8
Igor Vernik <igorV@bsihq.com>
2019-05-08 12:40:20 -0400

    Adjusted imports.

--- src/main/java/com/bsi/sec/svc/TPFDataPuller.java
+++ src/main/java/com/bsi/sec/svc/TPFDataPuller.java
@@ -5,8 +5,8 @@
  */
 package com.bsi.sec.svc;
 
-import com.bsi.sec.dto.DataSyncResponse;
 import com.bsi.sec.domain.Company;
+import com.bsi.sec.dto.DataSyncResponse;
 import com.bsi.sec.repository.CompanyRepository;
 import com.bsi.sec.tpfrepository.BtoCompRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +24,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TreeMap;
+import org.apache.ignite.IgniteAtomicSequence;
+
 /**
  *
  * Component is responsible for providing functionality to pull data from the
@@ -33,21 +35,24 @@ import java.util.TreeMap;
  */
 @Service
 public class TPFDataPuller implements DataSync {
-    
-     private final static Logger log = LoggerFactory.getLogger(TPFDataPuller.class);
-    
+
+    private final static Logger log = LoggerFactory.getLogger(TPFDataPuller.class);
+
     @Autowired
     private BtoCompRepository btoCompRepository;
-    
+
     @Autowired
-    private CompanyRepository companyRepository;
-    
-    
+    CompanyRepository companyRepository;
+
+    @Autowired
+    IgniteAtomicSequence getCompanyCacheSeq;
+
     /**
      * runInitialSync
+     *
      * @param fromDateTime
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
     @Override
     public DataSyncResponse runInitialSync(LocalDateTime fromDateTime) throws Exception {
@@ -56,70 +61,89 @@ public class TPFDataPuller implements DataSync {
         DataSyncResponse dataSyncResponse = upsertCompanyDataForSync(companyList);
         return dataSyncResponse;
     }
-    
+
     /**
      * runPeriodicSync
+     *
      * @param fromDateTime
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
     @Override
     public DataSyncResponse runPeriodicSync(LocalDateTime fromDateTime) throws Exception {
-        List<Btocomp> companies =  btoCompRepository.getAllComps();
-        System.out.println("companies "+companies);
-        if(companies !=null){
-            System.out.println("companies "+companies.size());
-            for (Btocomp company : companies) {
-            System.out.println(company.toString());
-            }
-        }
-        return null;
+        List<Btocomp> btoComplist = getCompanyDataForSync(fromDateTime);
+        List<Company> companyList = prepareCompanyList(btoComplist);
+        DataSyncResponse dataSyncResponse = upsertCompanyDataForSync(companyList);
+        return dataSyncResponse;
     }
-    
+
     /**
      * initializeSync
-     * @throws Exception 
+     *
+     * @throws Exception
      */
     @Override
     public void initializeSync() throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
     /**
      * getCompanyDataForSync
+     *
      * @param fromDateTime
-     * @return 
+     * @return
      */
     private List<Btocomp> getCompanyDataForSync(LocalDateTime fromDateTime) {
         List<Btocomp> btoComplist = null;
         if (fromDateTime == null) {
-            Calendar now = Calendar.getInstance();
-            now.set(Calendar.HOUR_OF_DAY, 23);
-            now.set(Calendar.MINUTE, 59);
-            now.set(Calendar.SECOND, 59);
-            log.debug("Current : " + now.get(Calendar.YEAR) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND));
-            now.add(Calendar.YEAR, -10);
-            log.debug("Current -10 Yrs : " + now.get(Calendar.YEAR) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND));
-            Date fromDate = now.getTime();
+            Date fromDate = getInitialSyncDateTime();
             btoComplist = btoCompRepository.getCompanyDataForSync(fromDate);
         } else {
-            Date fromDate = Date.from( fromDateTime.atZone( ZoneId.systemDefault()).toInstant());
+            Date fromDate = getPeriodicSyncDateTime(fromDateTime);
             btoComplist = btoCompRepository.getCompanyDataForSync(fromDate);
-            System.out.println(btoComplist.size());
-            System.out.println(btoComplist.get(0).getSamlcid());
         }
         return btoComplist;
     }
-    
+
+    /**
+     * getInitialSyncDateTime
+     *
+     * @return
+     */
+    private Date getInitialSyncDateTime() {
+        Calendar now = Calendar.getInstance();
+        now.set(Calendar.HOUR_OF_DAY, 23);
+        now.set(Calendar.MINUTE, 59);
+        now.set(Calendar.SECOND, 59);
+        log.debug("Current : " + now.get(Calendar.YEAR) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND));
+        now.add(Calendar.YEAR, -10);
+        log.debug("Current -10 Yrs : " + now.get(Calendar.YEAR) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND));
+        Date fromDate = now.getTime();
+        return fromDate;
+    }
+
+    /**
+     * getPeriodicSyncDateTime
+     *
+     * @param fromDateTime
+     * @return
+     */
+    private Date getPeriodicSyncDateTime(LocalDateTime fromDateTime) {
+        Date fromDate = Date.from(fromDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        return fromDate;
+    }
+
     /**
      * prepareCompanyList
+     *
      * @param btoComplist
-     * @return 
+     * @return
      */
     private List<Company> prepareCompanyList(List<Btocomp> btoComplist) {
         log.debug("Inside prepareCompanyList");
-        log.debug("BtoComp Count : "+btoComplist.size());
+        log.debug("BtoComp Count : " + btoComplist.size());
         List<Company> companyList = new ArrayList<>();
-        for (Btocomp btocomp : btoComplist) {
+        btoComplist.stream().map((btocomp) -> {
             Company company = new Company();
             company.setDataset(btocomp.getBtodset().getName());
             company.setEnabled(Boolean.TRUE);
@@ -128,22 +152,37 @@ public class TPFDataPuller implements DataSync {
             company.setImportedDate(Instant.EPOCH);
             company.setName(btocomp.getLegalname());
             company.setSamlCid(btocomp.getSamlcid());
+            return company;
+        }).forEachOrdered((company) -> {
             companyList.add(company);
-        }
-        log.debug("Returning  Company List count: "+companyList.size());
+        });
+        log.debug("Returning  Company List count: " + companyList.size());
         return companyList;
     }
-    
+
     /**
      * upSertCompanyDataForSync
+     *
      * @param compnatList
-     * @return 
+     * @return
      */
-    private DataSyncResponse upsertCompanyDataForSync(List<Company> companyList){
+    private DataSyncResponse upsertCompanyDataForSync(List<Company> companyList) {
         log.debug("Inside upsertCompanyDataForSync : ");
-        log.debug("Received Company Count : "+companyList.size());
-        companyRepository.saveAll(companyList);
-        System.out.println("\n>>> Added " + companyRepository.count() + " Persons into the repository.");
-        return null;
+        log.debug("Received Company Count : " + companyList.size());
+
+        TreeMap<Long, Company> companies = new TreeMap<>();
+        companyList.forEach((company) -> {
+            Long id = getCompanyCacheSeq.incrementAndGet();
+            companies.put(id, company);
+        });
+        companyRepository.save(companies);
+
+        log.debug("Added " + companyRepository.count() + " Companies into the Company repository.");
+
+        DataSyncResponse dataSyncResponse = new DataSyncResponse();
+        dataSyncResponse.setIsSucessfull(Boolean.TRUE);
+        dataSyncResponse.setLastRunDateTime(LocalDateTime.now());
+        dataSyncResponse.setMessage("TPF Data Synced Successfully.");
+        return dataSyncResponse;
     }
 }
